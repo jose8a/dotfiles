367cd9f589d668c1fe23de79c2b22b14a8ffeca3
jose8a <ochovilla@gmail.com>
2020-02-11 19:01:47 -0800

    Misc updates

--- setup.sh
+++ setup.sh
@@ -15,6 +15,10 @@ find . -maxdepth 1 -name ".*" | while read fname; do
   fi
 done
 
+# TODO: handle the .vimconfig directory as well
+#   - `rmdir $HOME/.vimconfig`
+#   - `ls -s ...etc`
+
 # create dynamic file to store the latest location of dotfiles folder
 # needs to be recreated every time the DOTFILES/setup.h is run in
 # case location of dotfiles local repo changes
